76d1e704697d9b70d7681164c9d7eb7c3316df53
Jack Vang <jvs@visiolink.com>
2024-01-18 15:26:13 +0100

    first commit

--- Sources/PasskeysManager/PasskeysManager.swift
+++ Sources/PasskeysManager/PasskeysManager.swift
@@ -1,2 +1,92 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import AuthenticationServices
+import Foundation
+import os
+
+extension NSNotification.Name {
+    static let UserSignedIn = Notification.Name("UserSignedInNotification")
+    static let ModalSignInSheetCanceled = Notification.Name("ModalSignInSheetCanceledNotification")
+}
+
+@available(macOS 12.0, *)
+public class AccountManager: NSObject, ASAuthorizationControllerPresentationContextProviding, ASAuthorizationControllerDelegate {
+
+    var authenticationAnchor: ASPresentationAnchor?
+    var isPerformingModalReqest = false
+
+    public func authorizationController(controller: ASAuthorizationController, didCompleteWithAuthorization authorization: ASAuthorization) {
+        let logger = Logger()
+        switch authorization.credential {
+        case let credentialRegistration as ASAuthorizationPlatformPublicKeyCredentialRegistration:
+            logger.log("A new passkey was registered: \(credentialRegistration)")
+            // Verify the attestationObject and clientDataJSON with your service.
+            // The attestationObject contains the user's new public key to store and use for subsequent sign-ins.
+            // let attestationObject = credentialRegistration.rawAttestationObject
+            // let clientDataJSON = credentialRegistration.rawClientDataJSON
+
+            // After the server verifies the registration and creates the user account, sign in the user with the new account.
+            didFinishSignIn()
+        case let credentialAssertion as ASAuthorizationPlatformPublicKeyCredentialAssertion:
+            logger.log("A passkey was used to sign in: \(credentialAssertion)")
+            // Verify the below signature and clientDataJSON with your service for the given userID.
+            // let signature = credentialAssertion.signature
+            //let clientDataJSON = credentialAssertion.rawClientDataJSON
+            let userID = credentialAssertion.userID
+            
+            let str = String(decoding: userID!, as: UTF8.self)
+            
+            Task {
+                
+            }
+        case let passwordCredential as ASPasswordCredential:
+            logger.log("A password was provided: \(passwordCredential)")
+            // Verify the userName and password with your service.
+            // let userName = passwordCredential.user
+            // let password = passwordCredential.password
+
+            // After the server verifies the userName and password, sign in the user.
+            didFinishSignIn()
+        default:
+            fatalError("Received unknown authorization type.")
+        }
+
+        isPerformingModalReqest = false
+    }
+
+    public func authorizationController(controller: ASAuthorizationController, didCompleteWithError error: Error) {
+        let logger = Logger()
+        guard let authorizationError = error as? ASAuthorizationError else {
+            isPerformingModalReqest = false
+            logger.error("Unexpected authorization error: \(error.localizedDescription)")
+            return
+        }
+
+        if authorizationError.code == .canceled {
+            // Either the system doesn't find any credentials and the request ends silently, or the user cancels the request.
+            // This is a good time to show a traditional login form, or ask the user to create an account.
+            logger.log("Request canceled.")
+
+            if isPerformingModalReqest {
+                didCancelModalSheet()
+            }
+        } else {
+            // Another ASAuthorization error.
+            // Note: The userInfo dictionary contains useful information.
+            logger.error("Error: \((error as NSError).userInfo)")
+        }
+
+        isPerformingModalReqest = false
+    }
+
+
+    public func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return authenticationAnchor!
+    }
+
+    func didFinishSignIn() {
+        NotificationCenter.default.post(name: .UserSignedIn, object: nil)
+    }
+
+    func didCancelModalSheet() {
+        NotificationCenter.default.post(name: .ModalSignInSheetCanceled, object: nil)
+    }
+}
